eff74b1de16e6d3112de1219af4e57c6ee08e691
suriya3153 <107272107+suriya3153@users.noreply.github.com>
2024-10-06 18:06:47 +0530

    Update web_page.py

--- web_page.py
+++ web_page.py
@@ -1,17 +1,60 @@
+# import the streamlit library
 import streamlit as st
-import whisper
 
-st.title("Audio to Text Converter")
+# give a title to our app
+st.title('Welcome to BMI Calculator')
 
-audio_file=st.file_uploader("upload audio file")
+# TAKE WEIGHT INPUT in kgs
+weight = st.number_input("Enter your weight (in kgs)")
 
-if audio_file is not None:
-    model=whisper.load_model("base")
-    st.write("file uploaded")
-    with open("audio_temp.wav","wb") as f:
-        f.write(audio_file.getbuffer())
-    st.audio(audio_file)
-    with st.spinner("Converting to Text"):
-        result=model.transcribe("audio_temp.wav")
-    st.write(result["language"])
-    st.write(result["text"])
+# TAKE HEIGHT INPUT
+# radio button to choose height format
+status = st.radio('Select your height format: ',
+                  ('cms', 'meters', 'feet'))
+
+# compare status value
+if(status == 'cms'):
+    # take height input in centimeters
+    height = st.number_input('Centimeters')
+
+    try:
+        bmi = weight / ((height/100)**2)
+    except:
+        st.text("Enter some value of height")
+
+elif(status == 'meters'):
+    # take height input in meters
+    height = st.number_input('Meters')
+
+    try:
+        bmi = weight / (height ** 2)
+    except:
+        st.text("Enter some value of height")
+
+else:
+    # take height input in feet
+    height = st.number_input('Feet')
+
+    # 1 meter = 3.28
+    try:
+        bmi = weight / (((height/3.28))**2)
+    except:
+        st.text("Enter some value of height")
+
+# check if the button is pressed or not
+if(st.button('Calculate BMI')):
+
+    # print the BMI INDEX
+    st.text("Your BMI Index is {}.".format(bmi))
+
+    # give the interpretation of BMI index
+    if(bmi < 16):
+        st.error("You are Extremely Underweight")
+    elif(bmi >= 16 and bmi < 18.5):
+        st.warning("You are Underweight")
+    elif(bmi >= 18.5 and bmi < 25):
+        st.success("Healthy")
+    elif(bmi >= 25 and bmi < 30):
+        st.warning("Overweight")
+    elif(bmi >= 30):
+        st.error("Extremely Overweight")
